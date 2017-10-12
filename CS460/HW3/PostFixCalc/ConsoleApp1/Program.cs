eeddce9f21f7d28115aa8305a89ba8a67b86e88d
Stephen Oliver <skoliver89@gmail.com>
2017-10-11 19:12:56 -0700

    Started work on the main calculator method

--- CS460/HW3/PostFixCalc/ConsoleApp1/Program.cs
+++ CS460/HW3/PostFixCalc/ConsoleApp1/Program.cs
@@ -8,8 +8,51 @@ namespace PostFixCalc
 {
     class Program
     {
+        //Create a new empty stack
+        private LinkedStack<double> stack = new LinkedStack<double>();
+
+        /// <summary>
+        /// The main method for the PostFix Calculator
+        /// </summary>
+        /// <param name="args">Comand Line Args (unused)</param>
         static void Main(string[] args)
         {
+            //Tell the user what is expected
+            Console.WriteLine("\nPostfix Calculator. Recognizes these operators: + - * /");
+            bool playAgain = true;
+            while(playAgain)
+            {
+                playAgain = doCalculation();
+            }
+            Console.WriteLine("Bye.");            
+        }
+
+        static bool doCalculation()
+        {
+            Console.WriteLine("Please enter q to quit\n");
+            Console.Write("> "); //Prompt the User
+
+            //Get space deliminated user input and add to an array of strings
+            string[] input = Console.ReadLine().Split(' ');
+
+            //See if the user wishes to quit
+            if(input[0].StartsWith("q") || input[0].StartsWith("Q"))
+            {
+                return false;
+            }
+
+            //Go go gadget calculator!
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
+
+
+            return true;
         }
     }
 }
