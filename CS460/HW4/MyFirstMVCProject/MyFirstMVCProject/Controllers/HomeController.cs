8f98584c1efd442d5891b2d39dd9ea479afd4c38
Stephen Oliver <skoliver89@gmail.com>
2017-10-18 16:45:22 -0700

    Added the remaining conversions; Page Logic Finished

--- CS460/HW4/MyFirstMVCProject/MyFirstMVCProject/Controllers/HomeController.cs
+++ CS460/HW4/MyFirstMVCProject/MyFirstMVCProject/Controllers/HomeController.cs
@@ -34,36 +34,38 @@ namespace MyFirstMVCProject.Controllers
             {
                 //Convert the given temp to a double
                 double t = Double.Parse(temperature);
-                //constant for F to C and F to K
-                double x = 5.00 / 9.00;
-                //constant for F to N
-                double y = 11.00 / 60.00;
                 //Convert to F, C, and/or K depending on what unit is supplied
                 //Formulas found here: https://en.wikipedia.org/wiki/Conversion_of_units_of_temperature
                 switch (units)
                 {
                     case "F":
-                        ViewBag.a = ((t - 32.00) * x).ToString();
-                        ViewBag.b = ((t + 459.67) * x).ToString();
-                        ViewBag.c = ((t - 32.00) * y).ToString();
+                        ViewBag.a = ((t - 32.00) * (5.00 / 9.00)).ToString();   //F to C
+                        ViewBag.b = ((t + 459.67) * (5.00 / 9.00)).ToString();  //F to K
+                        ViewBag.c = ((t - 32.00) * (11.00 / 60.00)).ToString(); //F to N
                         ViewBag.aU = "C"; //send the unit label for Celsius
                         ViewBag.bU = "K"; //send the unit label for Kelvin
                         ViewBag.cU = "N"; //send the unit label for Newtons
                         break;
                     case "C":
-
+                        ViewBag.a = (t * (5.00 / 9.00) + 32.00).ToString();  //C to F
+                        ViewBag.b = (t + 273.15).ToString();                 //C to K
+                        ViewBag.c = (t * (33.00/100.00)).ToString();         //C to N
                         ViewBag.aU = "F"; //send the unit label for Fahrenheit
                         ViewBag.bU = "K"; //send the unit label for Kelvin
                         ViewBag.cU = "N"; //send the unit label for Newtons
                         break;
                     case "K":
-
+                        ViewBag.a = (t * (5.00 / 9.00) - 459.67).ToString();      //K to F
+                        ViewBag.b = (t - 273.15).ToString();                      //K to C
+                        ViewBag.c = ((t - 273.15) * (33.00 / 100.00)).ToString(); //K to N
                         ViewBag.aU = "F"; //send the unit label for Fahrenheit
                         ViewBag.bU = "C"; //send the unit label for Celsius
                         ViewBag.cU = "N"; //send the unit label for Newtons
                         break;
                     case "N":
-
+                        ViewBag.a = (t * (60.00 / 11.00) + 32.00).ToString();   //N to F
+                        ViewBag.b = (t * (100.00 / 33.00)).ToString();          //N to C
+                        ViewBag.c = (t * (100.00 / 33.00) + 273.15).ToString(); //N to K
                         ViewBag.aU = "F"; //send the unit label for Fahrenheit
                         ViewBag.bU = "C"; //send the unit label for Celsius
                         ViewBag.cU = "K"; //send the unit label for Kelvin
