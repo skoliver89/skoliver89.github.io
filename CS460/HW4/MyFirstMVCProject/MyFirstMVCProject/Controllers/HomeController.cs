f9c1b13b1c33b1411558011eb36495b3c599e0a2
Stephen Oliver <skoliver89@gmail.com>
2017-10-18 16:05:24 -0700

    added the F to C, K, and N conversion

--- CS460/HW4/MyFirstMVCProject/MyFirstMVCProject/Controllers/HomeController.cs
+++ CS460/HW4/MyFirstMVCProject/MyFirstMVCProject/Controllers/HomeController.cs
@@ -22,6 +22,43 @@ namespace MyFirstMVCProject.Controllers
         // GET ~/Home/Page1
         public ActionResult Page1()
         {
+            // Get parameter values from the client via query strings
+            string temperature = Request.QueryString["temperature"];
+            string units = Request.QueryString["units"];
+            // Build a new page with the param values
+            //return what the user put into the form to the newpage
+            ViewBag.temperature = temperature;
+            ViewBag.units = units;
+
+            if (temperature != null && temperature != "")
+            {
+                //Convert the given temp to a double
+                double t = Double.Parse(temperature);
+                //constant for F to C and F to K
+                double x = 5.00 / 9.00;
+                //constant for F to N
+                double y = 11.00 / 60.00;
+                //Convert to F, C, and/or K depending on what unit is supplied
+                switch(units)
+                {
+                    case "F":
+                        ViewBag.a = ((t - 32.00) * x).ToString();
+                        ViewBag.b = ((t + 459.67) * x).ToString();
+                        ViewBag.c = ((t - 32.00) * y).ToString();
+                        ViewBag.aU = "C"; //send the unit label for Celsius
+                        ViewBag.bU = "K"; //send the unit label for Kelvin
+                        ViewBag.cU = "N"; //send the unit label for Newtons
+                        break;
+                    case "C":
+
+                        break;
+                    case "K":
+
+                        break;
+                }
+            }
+
+
             return View();
         }
     }
