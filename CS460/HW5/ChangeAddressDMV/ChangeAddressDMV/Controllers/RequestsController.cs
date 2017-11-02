82501ce7d85c1bbbadb3abe9c81495584b6a54fb
Stephen Oliver <skoliver89@gmail.com>
2017-11-02 15:15:52 -0700

    Added controller and action method (idex) for current requests

---
+++ CS460/HW5/ChangeAddressDMV/ChangeAddressDMV/Controllers/RequestsController.cs
@@ -0,0 +1,23 @@
+﻿using ChangeAddressDMV.DAL;
+using ChangeAddressDMV.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChangeAddressDMV.Controllers
+{
+    public class RequestsController : Controller
+    {
+        //db variable
+        private RequestContext db = new RequestContext();
+
+        // GET: Requests
+        // Listing all current requests in the index
+        public ActionResult Index()
+        {
+            return View(db.Requests.ToList());
+        }
+    }
+}
