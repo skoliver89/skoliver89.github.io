c0a0e3ceb420220a736e49b90c3001bd81db85d8
Stephen Oliver <skoliver89@gmail.com>
2017-11-01 18:51:58 -0700

    created up script for sql table; no example entries

---
+++ CS460/HW5/ChangeAddressDMV/ChangeAddressDMV/App_Data/up.sql
@@ -0,0 +1,14 @@
+-- Requests Table
+CREATE TABLE dbo.Requests
+(
+	RequestID			INT IDENTITY (1,1) NOT NULL,
+	IDNumber			INT	NOT NULL,
+	DOB					Date	NOT NULL,
+	FullName			NVARCHAR(128)	NOT NULL,
+	NewResAddr			NVARCHAR(128)	NOT NULL,
+	NewResCityStateZip	NVARCHAR(128) NOT NULL,
+	NewResCounty		NVARCHAR(128)	NOT NULL,
+	NewMailAddr			NVARCHAR(128),
+	NewMailCityStateZip	NVARCHAR(128),
+	NewMailCounty		NVARCHAR(128)
+);
