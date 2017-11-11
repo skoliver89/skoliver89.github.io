08c15c183ea1e9e60ecff39b4c3298da5044c8b3
Stephen Oliver <skoliver89@gmail.com>
2017-11-11 00:08:58 -0800

    End of night commit

---
+++ CS460/HW6/AdventureWorks/AdventureWorks/Controllers/CatalogController.cs
@@ -0,0 +1,21 @@
+﻿using AdventureWorks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdventureWorks.Controllers
+{
+    public class CatalogController : Controller
+    {
+        //interface to the DB
+        AdventureWorks2014Context db = new AdventureWorks2014Context();
+        // GET: Catalog
+        public ActionResult Index()
+        {
+            //Get a list of Catagories to Return to the View
+            return View(db.ProductCategories);
+        }
+    }
+}
