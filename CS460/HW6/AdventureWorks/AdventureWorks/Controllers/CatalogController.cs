702aa8a2d2f37d3d76cb850181a6b76811979c68
Stephen Oliver <skoliver89@gmail.com>
2017-11-11 16:53:24 -0800

    Added category methods/views and links on navbar and all categories page

--- CS460/HW6/AdventureWorks/AdventureWorks/Controllers/CatalogController.cs
+++ CS460/HW6/AdventureWorks/AdventureWorks/Controllers/CatalogController.cs
@@ -9,13 +9,41 @@ namespace AdventureWorks.Controllers
 {
     public class CatalogController : Controller
     {
-        //interface to the DB
+        //interface to the DB/models
         AdventureWorks2014Context db = new AdventureWorks2014Context();
+
         // GET: Catalog
         public ActionResult Index()
         {
-            //Get a list of Catagories to Return to the View
-            return View(db.ProductCategories);
+            return View(db.ProductSubcategories);
+        }
+
+        // GET: Catalog/Accessories
+        public ActionResult Accessories()
+        {
+            var subCats = db.ProductSubcategories.Where(n => n.ProductCategory.Name == "Accessories");
+            return View(subCats);
+        }
+
+        // GET: Catalog/Bikes
+        public ActionResult Bikes()
+        {
+            var subCats = db.ProductSubcategories.Where(n => n.ProductCategory.Name == "Bikes");
+            return View(subCats);
+        }
+
+        // GET: Catalog/Components
+        public ActionResult Components()
+        {
+            var subCats = db.ProductSubcategories.Where(n => n.ProductCategory.Name == "Components");
+            return View(subCats);
+        }
+
+        // GET: Catalog/Clothing
+        public ActionResult Clothing()
+        {
+            var subCats = db.ProductSubcategories.Where(n => n.ProductCategory.Name == "Clothing");
+            return View(subCats);
         }
     }
 }
