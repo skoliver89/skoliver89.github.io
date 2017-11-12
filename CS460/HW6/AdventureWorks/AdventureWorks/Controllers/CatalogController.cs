06c45e78fc48a67aaec1bb5f64a3856c74b13c72
Stephen Oliver <skoliver89@gmail.com>
2017-11-12 02:52:19 -0800

    Finished basic catalog layout with product descripts

--- CS460/HW6/AdventureWorks/AdventureWorks/Controllers/CatalogController.cs
+++ CS460/HW6/AdventureWorks/AdventureWorks/Controllers/CatalogController.cs
@@ -1,6 +1,7 @@
 ﻿using AdventureWorks.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -21,29 +22,134 @@ namespace AdventureWorks.Controllers
         // GET: Catalog/Accessories
         public ActionResult Accessories()
         {
-            var subCats = db.ProductSubcategories.Where(n => n.ProductCategory.Name == "Accessories");
-            return View(subCats);
+            List<string> subCats = db.ProductSubcategories.Where(n => n.ProductCategory.Name == "Accessories").Select(sn => sn.Name).ToList();
+            ViewBag.subCats = subCats;
+
+            return View();
+        }
+        // POST: Catalog/Accessories
+        [HttpPost]
+        public ActionResult Accessories(string subCat)
+        {
+            //Maintain the Bike Catalog buttons
+            List<string> subCats = db.ProductSubcategories.Where(n => n.ProductCategory.Name == "Accessories").Select(sn => sn.Name).ToList();
+            ViewBag.subCats = subCats;
+
+            //find the products in the sub category
+            var products = db.Products.Where(s => s.ProductSubcategory.Name == subCat
+                                                    && s.FinishedGoodsFlag == true
+                                                    && s.DiscontinuedDate == null
+                                                    && s.SellEndDate == null);
+            if (products.Count() > 0)
+            {
+                return View(products);
+            }
+            return RedirectToAction("Accessories");
         }
 
         // GET: Catalog/Bikes
         public ActionResult Bikes()
         {
-            var subCats = db.ProductSubcategories.Where(n => n.ProductCategory.Name == "Bikes");
-            return View(subCats);
-        }
+            List<string> subCats = db.ProductSubcategories.Where(n => n.ProductCategory.Name == "Bikes").Select(sn => sn.Name).ToList();
+            ViewBag.subCats = subCats;
 
+            var products = db.Products;
+            return View(products);
+        }
+        // POST: Catalog/Bikes
+        [HttpPost]
+        public ActionResult Bikes(string subCat)
+        {
+            //Maintain the Bike Catalog buttons
+            List<string> subCats = db.ProductSubcategories.Where(n => n.ProductCategory.Name == "Bikes").Select(sn => sn.Name).ToList();
+            ViewBag.subCats = subCats;
+
+            //find the products in the sub category
+            var products = db.Products.Where(s => s.ProductSubcategory.Name == subCat
+                                                    && s.FinishedGoodsFlag == true
+                                                    && s.DiscontinuedDate == null
+                                                    && s.SellEndDate == null);
+
+            if (products.Count() > 0)
+            {
+                return View(products);
+            }
+            return RedirectToAction("Bikes");
+        }
+    
         // GET: Catalog/Components
         public ActionResult Components()
         {
-            var subCats = db.ProductSubcategories.Where(n => n.ProductCategory.Name == "Components");
-            return View(subCats);
-        }
+            List<string> subCats = db.ProductSubcategories.Where(n => n.ProductCategory.Name == "Components").Select(sn => sn.Name).ToList();
+            ViewBag.subCats = subCats;
 
+            return View();
+        }
+        // POST: Catalog/Components
+        [HttpPost]
+        public ActionResult Components(string subCat)
+        {
+            //Maintain the Bike Catalog buttons
+            List<string> subCats = db.ProductSubcategories.Where(n => n.ProductCategory.Name == "Components").Select(sn => sn.Name).ToList();
+            ViewBag.subCats = subCats;
+
+            //find the products in the sub category
+            var products = db.Products.Where(s => s.ProductSubcategory.Name == subCat
+                                                    && s.FinishedGoodsFlag == true
+                                                    && s.DiscontinuedDate == null
+                                                    && s.SellEndDate == null);
+            if (products.Count() > 0)
+            {
+                return View(products);
+            }
+            return RedirectToAction("Components");
+        }
         // GET: Catalog/Clothing
         public ActionResult Clothing()
         {
-            var subCats = db.ProductSubcategories.Where(n => n.ProductCategory.Name == "Clothing");
-            return View(subCats);
+            List<string> subCats = db.ProductSubcategories.Where(n => n.ProductCategory.Name == "Clothing" && n.Products.Count() > 0).Select(sn => sn.Name).ToList();
+            ViewBag.subCats = subCats;
+
+            return View();
+        }
+        // POST: Catalog/Clothing
+        [HttpPost]
+        public ActionResult Clothing(string subCat)
+        {
+            //Maintain the Bike Catalog buttons
+            List<string> subCats = db.ProductSubcategories.Where(n => n.ProductCategory.Name == "Clothing" && n.Products.Count() > 0).Select(sn => sn.Name).ToList();
+            ViewBag.subCats = subCats;
+
+            //find the products in the sub category
+            var products = db.Products.Where(s => s.ProductSubcategory.Name == subCat
+                                                    && s.FinishedGoodsFlag == true
+                                                    && s.DiscontinuedDate == null
+                                                    && s.SellEndDate == null);
+            if (products.Count() > 0)
+            {
+                return View(products);
+            }
+            return RedirectToAction("Clothing");
+        }
+
+        // GET: Catalog/Product?id=<id>
+        //Display the details of a product
+        public ActionResult Product()
+        {
+            int id = Convert.ToInt32(Request.QueryString["id"]);
+
+            var product = db.Products.Where(p => p.ProductID == id).FirstOrDefault();
+            //Get the product Model ID for the Description.
+            int? pmi = product.ProductModelID;
+            //Get the english description
+            string desc = "Not Availiable";
+            if (pmi != null) //if there is a Product Model ID load in the description
+            {
+                desc = db.ProductModelProductDescriptionCultures.Where(p => p.ProductModelID == pmi).FirstOrDefault().ProductDescription.Description;
+            }
+            ViewBag.desc = desc;
+
+            return View(product);
         }
     }
 }
