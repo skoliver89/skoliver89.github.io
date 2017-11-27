8bd0d18e05b8be9d6a6917743d0682944a364519
Stephen Oliver <skoliver89@gmail.com>
2017-11-27 01:43:30 -0800

    Add an attribute to fix bug not allowing pop of Editfor on Date Type

--- CS460/HW8/ArtVault/ArtVault/Models/ArtistMetadata.cs
+++ CS460/HW8/ArtVault/ArtVault/Models/ArtistMetadata.cs
@@ -9,6 +9,8 @@ namespace ArtVault.Models
     public partial class ArtistMetadata
     {
         [DataType(DataType.Date)]
+        //Fix for editfor population: https://stackoverflow.com/questions/33247295/show-only-the-date-in-html-editorfor-helper
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [NoFutureDates(ErrorMessage = ("Future dates are not allowed."))]
         [Required]
         public DateTime BirthDate { get; set; }
