cbbf5f9e179082313781f30058ac35d9d95dbb73
Stephen Oliver <skoliver89@gmail.com>
2017-11-27 00:05:46 -0800

    Validation for Artists Model

---
+++ CS460/HW8/ArtVault/ArtVault/Models/ArtistMetadata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ArtVault.Models
+{
+    public partial class ArtistMetadata
+    {
+        [DataType(DataType.Date)]
+        [NoFutureDates(ErrorMessage = (nameof(BirthDate) + " must be no greater" +
+            " than today and no less than 110 years before today"))]
+        [Required]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [RegularExpression(@"(([A-z]+[' -]?[A-z]*)[ ]?([A-z]+[' -]?[A-z]*)?, )+([A-z]+)",
+            ErrorMessage ="Expected format :" +
+            " 'City, Country' or 'City, State, CountryCode'")]
+        public string BirthCity { get; set; }
+    }
+
+    [MetadataType(typeof(ArtistMetadata))]
+    public partial class Artist { }
+
+}
---
+++ CS460/HW8/ArtVault/ArtVault/Models/noFutureDates.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArtVault.Models
+{
+    //Inspired by: https://stackoverflow.com/questions/17321948/is-there-a-rangeattribute-for-datetime
+
+    /// <summary>
+    ///Custom Attribute to limit a DateTime/Date Field
+    ///Between Now() and Now() - 110 years
+    /// </summary>
+    public class NoFutureDates : RangeAttribute
+    {
+        public NoFutureDates() : base(typeof(DateTime),
+            DateTime.Now.AddYears(-110).ToShortDateString(),
+            DateTime.Now.ToShortDateString())
+        { }
+    }
+}
