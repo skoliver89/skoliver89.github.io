598dd52d9cb754d544e0dbd61e5157615b7d10a4
Stephen Oliver <skoliver89@gmail.com>
2017-11-26 21:08:42 -0800

    created views for artists, artworks, and classifications

---
+++ CS460/HW8/ArtVault/ArtVault/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using ArtVault.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArtVault.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Interface to DB
+        ArtVaultContext db = new ArtVaultContext();
+     
+        // GET: ~/
+        // GET: ~/Home
+        // GET: ~/Home/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET ~/Home/Artists
+        public ActionResult Artists()
+        {
+            List<ArtVault.Models.Artist> artists = db.Artists.ToList();
+            return View(artists);
+        }
+
+        //GET ~/Home/ArtWorks
+        public ActionResult ArtWorks()
+        {
+            var works = db.ArtWorks.ToList();
+            return View(works);
+        }
+
+        //GET ~/Home/Classifications
+        public ActionResult Classifications()
+        {
+            var classifications = db.Genres.ToList();
+            return View(classifications);
+        }
+    }
+}
---
+++ CS460/HW8/ArtVault/ArtVault/Models/ArtVaultContext.cs
@@ -0,0 +1,52 @@
+namespace ArtVault.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class ArtVaultContext : DbContext
+    {
+        public ArtVaultContext()
+            : base("name=ArtVaultContext")
+        {
+        }
+
+        public virtual DbSet<Artist> Artists { get; set; }
+        public virtual DbSet<ArtWork> ArtWorks { get; set; }
+        public virtual DbSet<Genre> Genres { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Artist>()
+                .Property(e => e.Name)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Artist>()
+                .Property(e => e.BirthCity)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Artist>()
+                .HasMany(e => e.ArtWorks)
+                .WithOptional(e => e.Artist1)
+                .HasForeignKey(e => e.Artist);
+
+            modelBuilder.Entity<ArtWork>()
+                .Property(e => e.Title)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<ArtWork>()
+                .Property(e => e.Artist)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<ArtWork>()
+                .HasMany(e => e.Genres)
+                .WithMany(e => e.ArtWorks)
+                .Map(m => m.ToTable("Classifications").MapLeftKey("ArtWork").MapRightKey("Genre"));
+
+            modelBuilder.Entity<Genre>()
+                .Property(e => e.Name)
+                .IsUnicode(false);
+        }
+    }
+}
